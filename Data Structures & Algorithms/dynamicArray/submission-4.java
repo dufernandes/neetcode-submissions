d89763c553bb2076d2d750ee1db1ff9d88d6013a
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2026-06-08 23:43:48 +0200

    Add: dynamicArray - submission-4

---
+++ Data Structures & Algorithms/dynamicArray/submission-4.java
@@ -0,0 +1,43 @@
+class DynamicArray {
+    private int[] array;
+    private int count;
+
+    public DynamicArray(int capacity) {
+        array = new int[capacity];
+    }
+
+    public int get(int i) {
+        return this.array[i];
+    }
+
+    public void set(int i, int n) {
+        this.array[i] = n;
+    }
+
+    public void pushback(int n) {
+        if (count == array.length) {
+            resize();
+        }
+        array[count++] = n;
+    }
+
+    public int popback() {
+        return array[count-- - 1];
+    }
+
+    private void resize() {
+        int[] newArray = new int[array.length * 2];
+        for (int index = 0; index < array.length; index++) {
+            newArray[index] = array[index];
+        }
+        array = newArray;
+    }
+
+    public int getSize() {
+        return count;
+    }
+
+    public int getCapacity() {
+        return array.length;
+    }
+}
