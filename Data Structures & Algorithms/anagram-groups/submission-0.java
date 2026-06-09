ca108d450cef4b53ed017f79120fd33e7df5aff4
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2026-06-09 15:15:03 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> anagrams = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            anagrams
+            .computeIfAbsent(new String(chars),
+                k -> new ArrayList())
+                    .add(str);
+        }
+
+        return new ArrayList<>(anagrams.values());
+    }
+}
