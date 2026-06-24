c3761fa57e9b01fe73b2381a5b38e5effddb1dc8
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2026-06-24 09:57:40 +0200

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+                continue;
+            }
+            char rightChar = s.charAt(right);
+            if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+                continue;
+            }
+            if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                return false;
+            }
+            left++; right--;
+        }
+        return true;
+    }
+}
