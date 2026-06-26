02750fc1272ab7b568fa246c58b234ef0a129008
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2026-06-26 21:30:57 +0200

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        int maxArea = 0;
+        for (int i = 0; i < heights.length - 1; i++) {
+            for (int j = 1; j < heights.length; j++) {
+                int tempArea = Math.min(heights[i], heights[j]) * (j - i);
+                maxArea = Math.max(tempArea, maxArea);
+            }
+        }
+
+        return maxArea;
+    }
+}
