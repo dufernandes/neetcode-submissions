df3425806167453aa6ce683b90bad2590f7665be
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2026-07-02 19:56:39 +0200

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int trap(int[] height) {
+        int volume = 0, maxLeft = height[0], maxRight = height[height.length - 1];
+        int left = 1, right = height.length - 2;
+
+        while (left <= right) {
+            if (maxLeft <= maxRight) {
+                volume += Math.max(0, maxLeft - height[left]);
+                maxLeft = Math.max(maxLeft, height[left]);
+                left++;
+            } else {
+                volume += Math.max(0, maxRight - height[right]);
+                maxRight = Math.max(maxRight, height[right]);
+                right--;
+            }
+        }
+
+        return volume;
+    }
+}
