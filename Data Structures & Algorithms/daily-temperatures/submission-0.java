2400f292fc395f5c7c85a9b87c35678fed26b521
Eduardo Antonio Cecilio Fernandes <eduardo.cecilio@gmail.com>
2026-08-31 15:26:02 +0200

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            result[i] = 0;
+            for (int j = i; j < temperatures.length; j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    result[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
